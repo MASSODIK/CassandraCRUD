ea349d557530908e4f74ea97d811a16f3d380da3
aqsal <muhammadaqsal96@gmail.com>
2022-12-19 13:20:27 +0700

    first commit

---
+++ src/main/java/com/kelompok6/mbdcassandra/MbdcassandraApplication.java
@@ -0,0 +1,13 @@
+package com.kelompok6.mbdcassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbdcassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbdcassandraApplication.class, args);
+	}
+
+}
